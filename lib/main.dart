7cfa2109111a02346ca97c99909d675b5984174d
Majid <majid.shahbaz@nxb.com.pk>
2024-05-29 01:30:24 +0500

    Splash setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:grocery_app/src/navigation/routes.dart';
+import 'package:grocery_app/src/ui/splash/splash.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,13 +12,20 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-     
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return GetMaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          initialRoute: Routes.splashRoute,
+          getPages: [
+            GetPage(name: Routes.splashRoute, page: () => const SplashPage()),
+          ],
+        );
+      },
     );
   }
 }
--- lib/src/ui/splash/splash.dart
+++ lib/src/ui/splash/splash.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
-class SplashPage extends StatefulWidget{
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
   @override
   State<StatefulWidget> createState() => SplashPageContent();
-  
 }
-class SplashPageContent extends State<SplashPage>{
+
+class SplashPageContent extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 100.h,
+      width: 100.h,
+      color: Colors.amber,
+    );
   }
-  
 }
