ef6a04ca653e36ba8416d27fbdd02f937e41742d
Majid <majid.shahbaz@nxb.com.pk>
2024-05-29 00:22:49 +0500

    Initial Commit

---
+++ lib/src/ui/splash/splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget{
+  const SplashPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => SplashPageContent();
+  
+}
+class SplashPageContent extends State<SplashPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+  
+}
